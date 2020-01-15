bac502c7c20000550ff1d0ebdd029cf3b9d13c58
Carl Parm <carlparm@gmail.com>
2020-01-15 20:23:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   array2 = []
     while counter < array.length do
-     square = (array[counter**2])
+     square = (array[counter]**2)
      array2.push(square)
     end
   return array2
